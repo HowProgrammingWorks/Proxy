1a9eae9a9f873106c6bb79318b92d8e6e3ec2b0c
tshemsedinov <timur.shemsedinov@gmail.com>
2018-02-28 01:47:11 +0200

    Example for function call interception

---
+++ JavaScript/7-apply.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const max = (a, b) => (a > b ? a : b);
+
+const amax = new Proxy(max, {
+  apply(target, context, args) {
+    console.log('apply', target.name, args);
+    return args.reduce(target);
+  }
+});
+
+console.log(amax(7, 3, 12, 5, 0, 4, 8, 5));
